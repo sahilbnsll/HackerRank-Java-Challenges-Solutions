ef8ff18721c459c11e5a8f7c25f86b2d51a2e92d
Sahil Bansal <bansalsahil740@gmail.com>
2021-02-18 02:30:12 +0530

    Solotion for java static initializer block HackerRank Problem

---
+++ Static-initializer-block.java
@@ -0,0 +1,18 @@
+
+    public static boolean flag = true;
+    public static int B,H;
+   static
+   {
+       Scanner sc= new Scanner(System.in);
+       B = sc.nextInt();
+       H = sc.nextInt();
+       if(B<0 || H<0){
+           flag=false;
+           System.out.println("java.lang.Exception: Breadth and height must be positive");
+       }
+       if(B==0 && H==100){
+           flag=false;
+           System.out.println("java.lang.Exception: Breadth and height must be positive");
+       }
+   } 
+
