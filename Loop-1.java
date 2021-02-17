e623358c1a85f26a94264afe0445f1bdbcf2217e
Sahil Bansal <bansalsahil740@gmail.com>
2021-02-18 02:17:30 +0530

    Solotion for java Loop-1 HackerRank Problem

---
+++ Loop-1.java
@@ -0,0 +1,40 @@
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int i;
+        int N = scanner.nextInt();
+        for(i=1;i<=10;++i)
+   import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.regex.*;
+
+        public class Solution {
+
+
+
+                    private static final Scanner scanner = new Scanner(System.in);
+
+                        public static void main(String[] args) {
+                                        int i;
+                                                int N
+~
+~
+~
+~
+Loop-1.java[+] [unix] (05:29 01/01/1970)                                                                                   35,19-54 Bot
+-- INSERT --
+
